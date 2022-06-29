7f8ab0e0bb7b0bc120d663611e77bcd1374d2461
benedetto597 <ejbg597@gmail.com>
2022-06-28 18:44:55 -0600

    v0.8

--- backend/src/main/java/io/benedetto/backend/repository/RepoCategory.java
+++
@@ -1,9 +0,0 @@
-package io.benedetto.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.benedetto.backend.model.Category;
-
-public interface RepoCategory extends JpaRepository<Category, Integer> {
-    public Category findById(int id);
-}
--- backend/src/main/java/io/benedetto/backend/repository/RepoProduct.java
+++
@@ -1,9 +0,0 @@
-package io.benedetto.backend.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.benedetto.backend.model.Product;
-
-public interface RepoProduct extends JpaRepository<Product, Integer> {
-    public Product findById(int id);
-}
