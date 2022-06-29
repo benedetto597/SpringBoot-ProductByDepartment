0d1a491262ccd36406f0e88a0f72adbe81c3a15a
Benedeπo <54255731+benedetto597@users.noreply.github.com>
2022-06-28 18:49:34 -0600

    Add files via upload

---
+++ src/test/java/io/benedetto/springboottoheroku/SpringboottoherokuApplicationTests.java
@@ -0,0 +1,13 @@
+package io.benedetto.springboottoheroku;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringboottoherokuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
