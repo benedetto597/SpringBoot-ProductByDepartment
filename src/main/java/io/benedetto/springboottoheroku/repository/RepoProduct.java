0142c1f00febb51bb9ea7eb832cdf0593f3ed833
benedetto597 <ejbg597@gmail.com>
2022-06-28 21:21:42 -0600

    v0.8

--- src/main/java/io/benedetto/springboottoheroku/repository/RepoProduct.java
+++
@@ -1,9 +0,0 @@
-package io.benedetto.springboottoheroku.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.benedetto.springboottoheroku.model.Product;
-
-public interface RepoProduct extends JpaRepository<Product, Integer> {
-    public Product findById(int id);
-}
