63e2c02d7aa6e594d8a6245640cf9acf248e47df
benedetto597 <ejbg597@gmail.com>
2022-06-28 21:21:22 -0600

    v0.8

--- src/main/java/io/benedetto/springboottoheroku/repository/RepoUser.java
+++
@@ -1,9 +0,0 @@
-package io.benedetto.springboottoheroku.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import io.benedetto.springboottoheroku.model.User;
-
-public interface RepoUser extends JpaRepository<User, Integer> {
-    public User findById(int id);
-}
