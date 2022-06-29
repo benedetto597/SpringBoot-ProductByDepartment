088afe02414e03991c359f06bef9454a0ff5693d
benedetto597 <ejbg597@gmail.com>
2022-06-28 21:16:34 -0600

    v0.8

--- src/main/java/io/benedetto/springboottoheroku/SpringboottoherokuApplication.java
+++ src/main/java/io/benedetto/springboottoheroku/SpringboottoherokuApplication.java
@@ -2,10 +2,18 @@ package io.benedetto.springboottoheroku;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
 
+@Controller
 @SpringBootApplication
 public class SpringboottoherokuApplication {
-
+	@RequestMapping("/")
+	@ResponseBody
+	String home() {
+	      return "Hello World!";
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(SpringboottoherokuApplication.class, args);
 	}
